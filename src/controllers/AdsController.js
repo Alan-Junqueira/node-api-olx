6163968bd9b70b7a654967da567b0286b065586b
Alan Junqueira <contato.alanjunqueira@gmail.com>
2023-01-09 12:27:43 -0300

    Create controller's functions

---
+++ src/controllers/AdsController.js
@@ -0,0 +1,7 @@
+module.exports = {
+  getCategories: async (req, res) => {},
+  addAction: async (req, res) => {},
+  getList: async (req, res) => {},
+  getIten: async (req, res) => {},
+  editAction: async (req, res) => {}
+};
