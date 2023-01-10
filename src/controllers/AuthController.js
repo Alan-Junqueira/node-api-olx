7493fe0b9a9252bee9c25eee11f6e91eaf33825a
Alan Junqueira <contato.alanjunqueira@gmail.com>
2023-01-10 10:43:45 -0300

    login with validation using bcrypt to validate password

--- src/controllers/AuthController.js
+++ src/controllers/AuthController.js
@@ -6,7 +6,33 @@ const User = require('../models/User');
 const State = require('../models/State');
 
 module.exports = {
-  signin: async (req, res) => {},
+  signin: async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.json({ error: errors.mapped() });
+    }
+
+    const data = matchedData(req);
+
+    const user = await User.findOne({ email: data.email });
+
+    if (!user) {
+      return res.json({ error: 'E-mail e/ou senha inválidos!' });
+    }
+
+    const match = await bcrypt.compare(data.password, user.passwordHash);
+    if (!match) {
+      return res.json({ error: 'E-mail e/ou senha inválidos!' });
+    }
+
+    const payload = (Date.now() + Math.random()).toString();
+    const token = await bcrypt.hash(payload, 10);
+
+    user.token = token;
+    await user.save();
+
+    return res.json({ token, email: data.email });
+  },
   signup: async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
