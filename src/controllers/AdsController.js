f4100aefac3d6ddea330b6272269277b04b09f05
Alan Junqueira <contato.alanjunqueira@gmail.com>
2023-01-10 12:16:58 -0300

    get all categories created

--- src/controllers/AdsController.js
+++ src/controllers/AdsController.js
@@ -1,5 +1,19 @@
+const Category = require('../models/Category');
+
 module.exports = {
-  getCategories: async (req, res) => {},
+  getCategories: async (req, res) => {
+    const cats = await Category.find();
+    let categories = [];
+
+    for (let i in cats) {
+      categories.push({
+        ...cats[i]._doc,
+        img: `${process.env.BASE}:${process.env.PORT}/assets/images/${cats[i].slug}.png`
+      });
+    }
+
+    return res.json({ categories });
+  },
   addAction: async (req, res) => {},
   getList: async (req, res) => {},
   getIten: async (req, res) => {},
