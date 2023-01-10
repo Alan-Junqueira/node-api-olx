55a368661c15a48b4e3079c4d4580581f10117a3
Alan Junqueira <contato.alanjunqueira@gmail.com>
2023-01-10 09:53:30 -0300

    Configure initial validation to signup

--- src/controllers/AuthController.js
+++ src/controllers/AuthController.js
@@ -1,4 +1,15 @@
+const { validationResult, matchedData } = require('express-validator');
+
 module.exports = {
   signin: async (req, res) => {},
-  signup: async (req, res) => {}
+  signup: async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.json({ error: errors.mapped() });
+    }
+
+    const data = matchedData(req);
+
+    res.json({ tudocerto: true, data });
+  }
 };
